62d53d93b4409c374f4f80f652302b66f5e88863
lfy2008 <lfy0811200111>
2018-05-02 18:38:24 +0800

    ok

--- cloud-gateway/src/main/java/com/cloudeye/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/cloudeye/CloudGatewayApplication.java
@@ -1,12 +1,21 @@
 package com.cloudeye;
 
+import com.cloudeye.gateway.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
+
+	@Bean
+    public AccessFilter accessFilter(){
+	    return new AccessFilter();
+    }
 }
---
+++ cloud-gateway/src/main/java/com/cloudeye/gateway/AccessFilter.java
@@ -0,0 +1,37 @@
+package com.cloudeye.gateway;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by lafangyuan on 2018/4/28.
+ */
+
+@Slf4j
+public class AccessFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return null;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return false;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        log.info("send {} request to {}",request.getMethod(),request.getRequestURL().toString());
+        return null;
+    }
+}
