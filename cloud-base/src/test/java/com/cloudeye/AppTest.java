2be2f913cdebecba7a1d879e2732e88ffda7b2e6
lfy2008 <lfy0811200111>
2019-04-19 09:09:45 +0800

    ok

--- cloud-base/src/test/java/com/cloudeye/AppTest.java
+++ cloud-base/src/test/java/com/cloudeye/AppTest.java
@@ -3,6 +3,10 @@ package com.cloudeye;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.springframework.util.StopWatch;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Unit test for simple App.
@@ -16,5 +20,34 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void wrapperEquals(){
+        Integer a = 100;
+        Integer b =100;
+        Integer c = new Integer(100);
+        System.out.println(a.equals(b));
+        System.out.println(a.equals(c));
+        System.out.println(a==b);
+        System.out.println(a==c);
+    }
+
+    @Test
+    public void mapCapacityExpansion(){
+        Map<Integer,Integer> a = new HashMap<>();
+        Map<Integer,Integer> b = new HashMap<>(10240);
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start("a");
+        for(int i=0;i<10240;i++){
+            a.put(i,i);
+        }
+        stopWatch.stop();
+        stopWatch.start("b");
+        for(int i=0;i<10240;i++){
+            b.put(i,i);
+        }
+        stopWatch.stop();
+        System.out.println(stopWatch.prettyPrint());
     }
 }
